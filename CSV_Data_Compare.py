0816cc6bfb9f9c723527ff9340906e1fc6ef07c7
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-08-07 15:38:46 +0800

    0.4.4

--- CSV_Data_Compare.py
+++ CSV_Data_Compare.py
@@ -34,12 +34,12 @@ input_file_1_path, input_file_1_encoding, key_1, input_file_2_path, input_file_2
 df1 = pd.read_csv(input_file_1_path, encoding=input_file_1_encoding)
 df2 = pd.read_csv(input_file_2_path, encoding=input_file_2_encoding)
 
-# For "未解决的记录", find the common keys in both files, and get the corresponding rows from File2
-common_keys = pd.merge(df1, df2, on=key_1)[key_1]
+# For "未解决的记录", find the keys that are in both files, and get all the corresponding rows from Input_File_2
+common_keys = set(df1[key_1]).intersection(set(df2[key_2]))
 unresolved_df = df2[df2[key_2].isin(common_keys)]
 unresolved_df.to_csv(output_file_1_path, encoding=output_file_1_encoding, index=False)
 
-# For "新增的记录", find the keys that are in File2 but not in File1, and get the corresponding rows from File2
+# For "新增的记录", find the keys that are in Input_File_2 but not in Input_File_1, and get all the corresponding rows from Input_File_2
 new_keys = set(df2[key_2]) - set(df1[key_1])
 new_df = df2[df2[key_2].isin(new_keys)]
 new_df.to_csv(output_file_2_path, encoding=output_file_2_encoding, index=False)
